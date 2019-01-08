b7ac074f930f6d7f237f4a6ae6d7adafd0cb0e99
hmmcclish <hmmcclish@gmail.com>
2019-01-08 08:15:47 -0800

    Do-AllTheThings - remove from Helpers
    Do-AllTheThings already in Substrate category.

--- DataGenerators/Public/Helpers.ps1
+++ DataGenerators/Public/Helpers.ps1
@@ -7,19 +7,3 @@ function Measure-TimeSpan ([DateTime]$Date){
 function Measure-Round([decimal]$Num){
 [Math]::Round($Num,2)
 }
-
-
-function Do-allTheThings {
-
-Param($port = 3000)
-
-
-Get-ActiveTCP | Send-ToElma -port $port
-Get-DriveInfo | Send-ToElma -port $port
-Get-LocalUsers | Send-ToElma -port $port
-Get-NetworkAdapterInfo | Send-ToElma -port $port
-Get-NetworkTotalTraffic | Send-ToElma -port $port
-Get-ProcessInfo | ConvertTo-Json | Send-ToElma -port $port
-Get-RDPSessions | Send-ToElma -SendEach -port $port
-Get-TitledWindows | ConvertTo-Json | Send-ToElma -port $port
-}
